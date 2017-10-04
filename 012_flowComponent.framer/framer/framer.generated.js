13e8dee73f382808573a5057b2774c3c8e5c3fe8
Natalie Marleny <nataliemarleny@gmail.com>
2017-10-04 21:43:56 +0100

    Add 012 flowcomponent starter gif

--- 012_flowComponent.framer/framer/framer.generated.js
+++ 012_flowComponent.framer/framer/framer.generated.js
@@ -7,7 +7,7 @@ if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","se
 }
 if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-7-black","contentScale":1,"hideBezel":true,"orientation":0};
 }
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"011_flowComponent.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"012_flowComponent.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();

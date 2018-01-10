1a3c657c1868e832f9867929936d6ce7f6dee6f9
Natalie Marleny <nataliemarleny@gmail.com>
2018-01-10 17:39:39 +0000

    Progress made to 012 flowComponent

--- 012_flowComponent/012_flowComponent.framer/framer/framer.generated.js
+++ 012_flowComponent/012_flowComponent.framer/framer/framer.generated.js
@@ -7,7 +7,7 @@ if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","se
 }
 if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-5s-space-gray","contentScale":1,"hideBezel":true,"orientation":0};
 }
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"012_flowComponent.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/\/resources.framerjs.com\/static\/DeviceResources","documentTitle":"012_flowComponent.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
